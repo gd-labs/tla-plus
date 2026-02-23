d2f977dd80f8d8a996cfd38768f9a88ac798a579
gustavodiasag <gustavodias.aguiar1@gmail.com>
2026-02-23 18:07:57 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# tla-plus
+
+
+# Reference
+
+https://lamport.azurewebsites.net/tla/book-21-07-04.pdf
---
+++ flake.nix
@@ -0,0 +1,57 @@
+{
+  description = "TLA+ development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils
+  }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        usrShell = builtins.getEnv "SHELL";
+
+        shell =
+          if usrShell != ""
+          then usrShell
+          else "${pkgs.bashInteractive}/bin/bash";
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            tlaplus
+            tlaplusToolbox
+            jdk
+            tlaps
+            gnumake
+          ];
+
+          shellHook = ''
+            export SHELL="${shell}"
+
+            echo "TLA+ development environment"
+            echo "Available tools:"
+            echo "  tlc2     - TLC model checker (CLI)"
+            echo "  tlatex   - TLATeX typesetter"
+            echo "  pcal     - PlusCal translator"
+            echo ""
+            echo "Example usage:"
+            echo "  tlc2 MySpec.tla"
+            echo "  pcal MySpec.tla && tlc2 MySpec.tla"
+
+            exec ${shell}
+          '';
+        };
+
+        apps.tlc2 = {
+          type = "app";
+          program = "${pkgs.tlaplus}/bin/tlc2";
+        };
+      });
+}
